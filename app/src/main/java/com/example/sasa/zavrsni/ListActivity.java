d45360619dd4b85825fc5ae76a959efc9ec61187
sasans <sasans@hotmail.com>
2016-11-26 20:55:12 +0100

    pred kraj  projekta

--- app/src/main/java/com/example/sasa/zavrsni/ListActivity.java
+++ app/src/main/java/com/example/sasa/zavrsni/ListActivity.java
@@ -1,31 +1,19 @@
 package com.example.sasa.zavrsni;
 
-import android.app.Dialog;
-import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.sasa.zavrsni.db.PripremaORMLightHelper;
 import com.example.sasa.zavrsni.db.model.Beleska;
@@ -33,7 +21,6 @@ import com.example.sasa.zavrsni.dilog.AboutDialog;
 import com.example.sasa.zavrsni.preferences.PrefererencesActivity;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -51,11 +38,6 @@ public class ListActivity extends AppCompatActivity {
     public static String NOTIF_TOAST = "notif_toast";
     public static String NOTIF_STATUS = "notif_statis";
 
-    private ImageView preview;
-    private String imagePath;
-
-    private static final int SELECT_PICTURE = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,7 +52,7 @@ public class ListActivity extends AppCompatActivity {
 
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-        final ListView listView = (ListView) findViewById(R.id.priprema_glumci_list);
+        final ListView listView = (ListView) findViewById(R.id.beleske_list);
 
         try {
             List<Beleska> list = getDatabaseHelper().getBeleskaDao().queryForAll();
@@ -108,7 +90,7 @@ public class ListActivity extends AppCompatActivity {
     }
 
     private void refresh() {
-        ListView listview = (ListView) findViewById(R.id.priprema_glumci_list);
+        ListView listview = (ListView) findViewById(R.id.beleske_list);
 
         if (listview != null){
             ArrayAdapter<Beleska> adapter = (ArrayAdapter<Beleska>) listview.getAdapter();
@@ -129,53 +111,6 @@ public class ListActivity extends AppCompatActivity {
         }
     }
 
-    private void showStatusMesage(String message){
-        NotificationManager mNotificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-        mBuilder.setSmallIcon(R.drawable.ic_launcher);
-        mBuilder.setContentTitle("Pripremni test");
-        mBuilder.setContentText(message);
-
-        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_add);
-
-        mBuilder.setLargeIcon(bm);
-        // notificationID allows you to update the notification later on.
-        mNotificationManager.notify(1, mBuilder.build());
-    }
-
-    private void selectPicture(){
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
-    }
-
-    /**
-     * Sismtemska metoda koja se automatksi poziva ako se
-     * aktivnost startuje u startActivityForResult rezimu
-     *
-     * Ako je ti slucaj i ako je sve proslo ok, mozemo da izvucemo
-     * sadrzaj i to da prikazemo. Rezultat NIJE sliak nego URI do te slike.
-     * Na osnovu toga mozemo dobiti tacnu putnaju do slike ali i samu sliku
-     * */
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_PICTURE) {
-                Uri selectedImageUri = data.getData();
-
-                try {
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
-                    imagePath = selectedImageUri.toString();
-
-                    if (preview != null){
-                        preview.setImageBitmap(bitmap);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
